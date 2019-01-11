4275b46347013fb3ecf87e2038a531f60ba73306
Joshua Arriola <joshuapatrickarriola@gmail.com>
2019-01-10 20:00:55 -0800

    Changing the game to Space Invaders Spongebob version

---
+++ js/game.js
@@ -0,0 +1,214 @@
+const KEY_CODE_LEFT = 37;
+const KEY_CODE_RIGHT = 39;
+const KEY_CODE_SPACE = 32;
+
+const GAME_WIDTH = 400;
+const GAME_HEIGHT = 600;
+
+const PLAYER_WIDTH = 20;
+const PLAYER_MAX_SPEED = 600;
+const LASER_MAX_SPEED = 300;
+const LASER_COOLDOWN = 0.1;
+
+const ENEMIES_PER_ROW = 6;
+const ENEMY_HORIZONTAL_PADDING = 80;
+const ENEMY_VERTICAL_PADDING = 70;
+const ENEMY_VERTICAL_SPACING = 80;
+
+const GAME_STATE = {
+  lastTime: Date.now(),
+  leftPressed: false,
+  rightPressed: false,
+  spacePressed: false,
+  playerX: 0,
+  playerY: 0,
+  playerCooldown: 0,
+  lasers: [],
+  enemies: [],
+};
+
+//intersection function
+function rectsIntersect(r1, r2) {
+  return !(
+    r2.left > r1.right ||
+    r2.right < r1.left ||
+    r2.top > r1.bottom ||
+    r2.bottom < r1.top 
+  );
+}
+//basically on hit
+
+function setPosition($el, x, y) {
+  $el.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+function clamp(v, min, max){
+  if ( v < min) {
+    return min;
+  } else if (v > max) {
+    return max;
+  } else {
+    return v;
+  }
+}
+
+function createPlayer($container){
+  GAME_STATE.playerX = GAME_WIDTH / 2; //x position of player
+  GAME_STATE.playerY = GAME_HEIGHT - 50; //bottom of the screen
+  const $player = document.createElement("img");
+  $player.src = "img/gary.png";
+  $player.className = "player";
+  $container.appendChild($player);
+  setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+}
+
+function init(){
+  const $container = document.querySelector(".game");
+  createPlayer($container);
+
+  //space invader enemies
+  const enemySpacing = (GAME_WIDTH - ENEMY_HORIZONTAL_PADDING * 2) /
+    (ENEMIES_PER_ROW - 1);
+  for (let j = 0; j < 3; j++) {
+    const y = ENEMY_VERTICAL_PADDING + j * ENEMY_VERTICAL_SPACING;
+    for (let i = 0; i < ENEMIES_PER_ROW; i++) {
+      const x = i * enemySpacing + ENEMY_HORIZONTAL_PADDING;
+      createEnemy($container, x, y);
+    }
+  }
+  //end of space invader enemies
+}
+
+function updatePlayer(dt, $container){
+  if (GAME_STATE.leftPressed){
+    GAME_STATE.playerX -= dt * PLAYER_MAX_SPEED;
+  }
+  if (GAME_STATE.rightPressed){
+    GAME_STATE.playerX += dt * PLAYER_MAX_SPEED;
+  }
+
+  GAME_STATE.playerX = clamp(GAME_STATE.playerX, PLAYER_WIDTH, GAME_WIDTH - PLAYER_WIDTH);
+  if (GAME_STATE.spacePressed && GAME_STATE.playerCooldown <= 0) {
+    createLaser($container, GAME_STATE.playerX, GAME_STATE.playerY);
+    GAME_STATE.playerCooldown = LASER_COOLDOWN;
+  }
+  if (GAME_STATE.playerCooldown > 0) {
+    GAME_STATE.playerCooldown -= dt;
+  }
+
+  const $player = document.querySelector(".player");
+  setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY); 
+}
+
+function createLaser($container, x, y) {
+  const $element = document.createElement("img");
+  $element.src = "img/laser-red-1.png";
+  $element.className = "laser";
+  $container.appendChild($element);
+  const laser = {x, y, $element};
+  GAME_STATE.lasers.push(laser);
+  setPosition($element, x, y);
+
+}
+
+function updateLasers(dt, $container){
+  const lasers = GAME_STATE.lasers;
+  for (let i = 0; i < lasers.length; i++) {
+    const laser = lasers[i];
+    laser.y -= dt * LASER_MAX_SPEED;
+    if (laser.y < 0){
+      destroyLaser($container, laser);
+    }
+    setPosition(laser.$element, laser.x, laser.y);
+    //enemy collision
+    const r1 = laser.$element.getBoundingClientRect();
+    const enemies = GAME_STATE.enemies;
+    for (let j = 0; j < enemies.length; j++) {
+      const enemy = enemies[j];
+      if (enemy.isDead) continue;
+      const r2 = enemy.$element.getBoundingClientRect();
+      if (rectsIntersect(r1, r2)) {
+        destroyEnemy($container, enemy);
+        destroyLaser($container, laser);
+        break;
+      }
+    }
+    //end of enemy collision
+  }
+  GAME_STATE.lasers = GAME_STATE.lasers.filter(l => !l.isDead);
+}
+
+function destroyLaser($container, laser){
+  $container.removeChild(laser.$element);
+  laser.isDead = true;
+}
+
+//space invader enemy
+function createEnemy($container, x, y){
+  const $element = document.createElement("img");
+  $element.src = "img/enemy-green-1.png";
+  $element.className = "enemy";
+  $container.appendChild($element);
+  const enemy = {x, y, $element};
+  GAME_STATE.enemies.push(enemy);
+  setPosition($element, x, y);
+}
+
+function updateEnemies(dt, $container){
+  const dx = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
+  const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
+
+  const enemies = GAME_STATE.enemies;
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
+    const x = enemy.x + dx;
+    const y = enemy.y + dy;
+    setPosition(enemy.$element, x, y);
+  }
+  GAME_STATE.enemies = GAME_STATE.enemies.filter(e => !e.isDead);
+}
+// end of space invader enemy
+
+function destroyEnemy($container, enemy){
+  $container.removeChild(enemy.$element);
+  enemy.isDead = true;
+}
+
+
+function update(){
+  const currentTime = Date.now();
+  const dt = (currentTime - GAME_STATE.lastTime) / 1000;
+
+  const $container = document.querySelector(".game");
+  updatePlayer(dt, $container);
+  updateLasers(dt, $container);
+  updateEnemies(dt, $container);
+
+  GAME_STATE.lastTime = currentTime;
+  window.requestAnimationFrame(update);
+}
+
+function onKeyDown(e) {
+  if (e.keyCode === KEY_CODE_LEFT) {
+    GAME_STATE.leftPressed = true;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = true;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = true;
+  }
+}
+
+function onKeyUp(e){
+  if(e.keyCode === KEY_CODE_LEFT){
+    GAME_STATE.leftPressed = false;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = false;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = false;
+  }
+}
+
+init();
+window.addEventListener("keydown", onKeyDown);
+window.addEventListener("keyup", onKeyUp);
+window.requestAnimationFrame(update);
